f68442ee33f014b9333bc6a143fbc99af682449f
Pravesh <kumarparvesh805.pk@gmail.com>
2024-10-29 00:36:17 +0530

    install mysql2 package to connect sql with node

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Practice repo of Using node.js with sql .
 # Install @faker-js/faker
 Install faker package to get fake data for practice.
 npm i @faker-js/faker 
+
+# Install mysql2 package
+To connect Node with MySQL.
+1. USE SQL IN CLI IN VS-CODE: 
+   open new terminal and run <b> mysql -u root -p </b>
+
+2. USE SQL IN VS-CODE BY FILE:
+   create <b>schema.sql</b> file. and write sql command in it.
+   for execute open mysql CLI by mysql -u root -p then USE datase and run <b>source schema.sql;</b> command for execute schema.sql file code.
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'users_db',
+    password: 'Pktanwar@123',
+});
+
+let q = `SHOW TABLES`;
+try{
+    connection.query(q, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+    });
+} catch(err){
+    console.log(err);
+}
+
+connection.end();
 
 let getRandomUser = () => {
     return {
@@ -8,5 +28,3 @@ let getRandomUser = () => {
       password: faker.internet.password(),
     };
   }
-
-  console.log(getRandomUser());
